1438a0026af51f7c156d3540d9b98732b87d9637
Shihai Wang <shihai@directivegames.com>
2016-04-05 11:29:51 +0800

    clean project

---
+++ Source/ClearPath/ClearPathGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ClearPathGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLEARPATH_API AClearPathGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
